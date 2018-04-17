06d32f4bd619f4fcbd353d7028339b8806ae2213
Hongbo Liu <hbliu@freewheel.tv>
2018-04-17 17:52:50 -0400

    add support for ubuntu

--- docker-compose-local/init_data.sh
+++ docker-compose-local/init_data.sh
@@ -5,10 +5,12 @@
 # Email: hbliu@freewheel.com
 # CreatTime: 2018-04-09 17:34:35 EDT
 
-rm -rf *data
-
 node_cnt=4
 tendermint_img="tendermint/tendermint:0.17.1"
+
+is_osx () {
+    [[ "$OSTYPE" =~ ^darwin ]] || return 1
+}
 
 init() {
     SED=sed
@@ -26,13 +28,29 @@ init() {
             sudo apt-get install jq -y
         fi
     fi
+
+    if is_osx; then
+        rm -rf *data
+    else
+        sudo rm -rf *data
+    fi
 }
 init
 
 default_genesis="./node1_data/config/genesis.json"
 
 for (( i = 1; i <= $node_cnt; i++ )); do
+    if ! is_osx; then
+        mkdir -p node${i}_data
+        chmod 777 node${i}_data
+    fi
+
     docker run --rm -v `pwd`/node${i}_data:/tendermint $tendermint_img init
+
+    if ! is_osx; then
+        sudo chmod -R 777 node${i}_data
+    fi
+
     node_id=$(docker run --rm -v `pwd`/node${i}_data:/tendermint $tendermint_img show_node_id)
     echo "Node$i ID: $node_id"
     $SED -i "s/[0-9a-f]\{40\}@tm_node$i/$node_id@tm_node$i/g" ./docker-compose.yml
