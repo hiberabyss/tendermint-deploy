5e2b6bf4ae82a8bc66486669bd8682c744aae58d
Hongbo Liu <hbliu@freewheel.tv>
2018-04-17 14:24:19 -0400

    optimize to update node id automatically

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 * `docker` contains some dockerfiles for building necessary docker images
 * `docker-compose-local` is about how to setup multiple tendermint nodes in local via docker-compose
+    * Pre-Requirement:
+        * `jq` needs to be installed with `brew install jq` or `sudo apt-get install jq`
+        * For Mac user, `gnu-sed` needs to be installed with `brew install gnu-sed`
     * Run `./init_data.sh` first;
     * Replace each node's ID in `docker-compose.yml` file
 * `k8s` is about how to setup tendermint cluster via kubernetes
--- docker-compose-local/init_data.sh
+++ docker-compose-local/init_data.sh
@@ -7,16 +7,26 @@
 
 rm -rf *data
 
-docker run --rm -v `pwd`/node1_data:/tendermint tendermint/tendermint init
-docker run --rm -v `pwd`/node2_data:/tendermint tendermint/tendermint init
+node_cnt=4
 
-echo "Node1 ID: $(docker run --rm -v `pwd`/node1_data:/tendermint tendermint/tendermint show_node_id)"
-echo "Node2 ID: $(docker run --rm -v `pwd`/node2_data:/tendermint tendermint/tendermint show_node_id)"
+SED=sed
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+    SED=gsed
+fi
 
-cat node2_data/config/genesis.json | jq ".validators |= .+ $(cat node1_data/config/genesis.json | jq '.validators')" > final_genesis.json
+default_genesis="./node1_data/config/genesis.json"
 
-cp ./final_genesis.json ./node2_data/config/genesis.json
-cp ./final_genesis.json ./node1_data/config/genesis.json
+for (( i = 1; i <= $node_cnt; i++ )); do
+    docker run --rm -v `pwd`/node${i}_data:/tendermint tendermint/tendermint init
+    node_id=$(docker run --rm -v `pwd`/node${i}_data:/tendermint tendermint/tendermint show_node_id)
+    echo "Node$i ID: $node_id"
+    $SED -i "s/[0-9a-f]\{40\}@tm_node$i/$node_id@tm_node$i/g" ./docker-compose.yml
 
-# gsed -i '/auth_enc/s/true/false/' node1_data/config/config.toml
-# gsed -i '/auth_enc/s/true/false/' node2_data/config/config.toml
+    if [[ $i != 1 ]]; then
+        echo $(cat $default_genesis | jq ".validators |= .+ $(cat node${i}_data/config/genesis.json | jq '.validators')") > $default_genesis
+    fi
+done
+
+for (( i = 2; i <= $node_cnt; i++ )); do
+    cp -f $default_genesis ./node${i}_data/config/genesis.json
+done
